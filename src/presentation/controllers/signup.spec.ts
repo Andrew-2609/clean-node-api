1106a94bc03fa22938805ead1953db0a69ad059d
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-03 12:44:00 -0300

    refactor: create and use barrel from 'protocols/' directory

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../errors'
-import { EmailValidator } from '../protocols/email-validator'
+import { EmailValidator } from '../protocols'
 import { SignUpController } from './signup'
 
 interface SutTypes {
