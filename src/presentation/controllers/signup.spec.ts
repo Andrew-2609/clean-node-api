2572e2ec80bdc42e094ada9faf45111a607f6205
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-03 10:49:49 -0300

    test: add test in case no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -17,4 +17,21 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toStrictEqual(new Error('Missing param: name'))
   })
+
+  test('should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'anyName',
+        password: 'anyPassword',
+        passwordConfirmation: 'anyPassword'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toStrictEqual(new Error('Missing param: email'))
+  })
 })
