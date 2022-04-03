6e962bb3ea46cb03223218a9f2385b32d767da81
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-03 10:30:29 -0300

    chore: import class SignUpController in signup.spec.ts

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,3 +1,5 @@
+import { SignUpController } from './signup'
+
 describe('SignUp Controller', () => {
   test('should return 400 if no name is provided', () => {
     const sut = new SignUpController()
