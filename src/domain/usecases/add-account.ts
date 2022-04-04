753bdc97b2395dd89f9b4ef915ff54844252d54e
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-03 21:46:13 -0300

    feat: implement and export new interface AddAccount with add() method signature

--- src/domain/usecases/add-account.ts
+++ src/domain/usecases/add-account.ts
@@ -1,6 +1,11 @@
+import { AccountModel } from '../models'
+
 export interface AddAccountModel {
   name: string
   emai: string
   password: string
 }
 
+export interface AddAccount {
+  add: (account: AddAccountModel) => AccountModel
+}
