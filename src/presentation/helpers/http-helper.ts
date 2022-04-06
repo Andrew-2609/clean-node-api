f4a625093673ddabc3df3223dd0c4f77689761ad
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-05 23:05:39 -0300

    refactor: optimize imports

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,5 +1,5 @@
 import { ServerError } from '../errors'
-import { HttpResponse } from '../protocols/http'
+import { HttpResponse } from '../protocols'
 
 export const ok = (data: any): HttpResponse => ({
   statusCode: 200,
