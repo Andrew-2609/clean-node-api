4d1821520532acca2dacd1da63371fadae396abf
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-07 22:23:03 -0300

    refactor: move db-account files to new folder 'add-accout/'

--- src/data/usescases/add-account/db-add-account.ts
+++ src/data/usescases/add-account/db-add-account.ts
@@ -1,6 +1,6 @@
-import { AccountModel } from '../../domain/models'
-import { AddAccount, AddAccountModel } from '../../domain/usecases'
-import { Encrypter } from '../protocols/encrypter'
+import { AccountModel } from '../../../domain/models'
+import { AddAccount, AddAccountModel } from '../../../domain/usecases'
+import { Encrypter } from '../../protocols/encrypter'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
