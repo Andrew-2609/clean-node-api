0c6a0abe7b9d93bc4349c56a78ccfdd2bce9b966
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-07 21:36:57 -0300

    chore: update collectCoverageFrom and coverageProvider properties

--- jest.config.ts
+++ jest.config.ts
@@ -3,9 +3,16 @@ import type { Config } from '@jest/types'
 const config: Config.InitialOptions = {
   roots: ['<rootDir>/src'],
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/src/domain/**/index.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
+  coverageProvider: 'babel',
   testEnvironment: 'jest-environment-node',
   transform: {
     '.+\\.ts$': 'ts-jest'
