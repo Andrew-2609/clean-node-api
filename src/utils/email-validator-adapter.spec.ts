fbed38ba36349b70ff71f34f8a0f1cd6dc55dd3f
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-07 21:45:08 -0300

    test: implement test to ensure isValid() returns false for invalid emails

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,11 @@
+import { EmailValidatorAdapter } from './email-validator'
+
+describe('EmailValidator Adapter', () => {
+  test('should return false if validator returns false', () => {
+    const sut = new EmailValidatorAdapter()
+
+    const isValid = sut.isValid('invalidEmail@mail.com')
+
+    expect(isValid).toBe(false)
+  })
+})
