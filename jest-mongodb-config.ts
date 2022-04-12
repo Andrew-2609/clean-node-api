1ba27ebaaa5ab3628be1ec6399a121f780556d13
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-11 23:22:54 -0300

    feat: configure jest for in memory mongodb

---
+++ jest-mongodb-config.ts
@@ -0,0 +1,12 @@
+export const mongodbMemoryServerOptions = {
+  mongodbMemoryServerOptions: {
+    instance: {
+      dbName: 'jest'
+    },
+    binary: {
+      version: '4.0.3',
+      skipMD5: true
+    },
+    autoStart: false
+  }
+}
