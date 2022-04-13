2c8324fa2a49d5fc7101bc594a33efdcf793a05f
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-13 07:05:37 -0300

    test: ensure cors is enabled in the application

---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Cors Middleware', () => {
+  test('should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send()
+    })
+
+    await request(app)
+      .get('/test_cors')
+      .expect('access-controll-allow-origin', '*')
+      .expect('access-controll-allow-methods', '*')
+      .expect('access-controll-allow-headers', '*')
+  })
+})
