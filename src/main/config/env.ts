c41858a6a1eeae8ba6ff4fdfd39fd45e385a099b
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-13 08:20:03 -0300

    chore: change mongoUrl

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
   port: process.env.port ?? 5050,
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-node-api'
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://172.17.0.2:27017/clean-node-api'
 }
