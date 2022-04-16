33bd344b4e8c71a2c99f06faffb53c88bd75daee
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-16 15:18:00 -0300

    refactor: simplify handle() function behavior for study purposes

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -8,10 +8,8 @@ export class LogControllerDecorator implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const httpResponse = await this.controller.handle(httpRequest)
-    if (httpResponse.statusCode === 500) {
-      console.error(httpResponse.body)
-    }
-    return httpResponse
+    await this.controller.handle(httpRequest)
+
+    return await new Promise(resolve => resolve({ statusCode: 200, body: {} }))
   }
 }
