30de667c0eaf713537b12cc74ace9436bdd575d0
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-16 15:50:24 -0300

    feat: add stack property to serverError

--- src/presentation/errors/server-error.ts
+++ src/presentation/errors/server-error.ts
@@ -1,6 +1,7 @@
 export class ServerError extends Error {
-  constructor () {
+  constructor (stack?: string) {
     super('An unexpected error has occurred. Please, try again later, or contact the developers!')
     this.name = 'ServerError'
+    this.stack = stack
   }
 }
