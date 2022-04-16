65f85ad0c511af3a90893d701cfd0d42d0da58f1
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-16 15:04:52 -0300

    refactor: generalize makeSignUpController return

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -2,9 +2,10 @@ import { DbAddAccount } from '../../data/usescases/add-account/db-add-account'
 import { BCryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
+import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const emailValidator = new EmailValidatorAdapter()
 
   const encryptAdapter = new BCryptAdapter(12)
